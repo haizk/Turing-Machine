02112331ba8cc333c4227a535da05adec49ecd8b
Haizk <haizkb@gmail.com>
2023-07-03 02:30:05 +0700

    placeholder, binarylog to 3 tape, ganti color asep

--- MultiTape/Multi2Tape.js
+++ MultiTape/Multi2Tape.js
@@ -123,9 +123,6 @@ function step() {
                 case "Square Root":
                     squareRoot();
                     break;
-                case "Binary Logarithm":
-                    binaryLogarithm();
-                    break;
             }
 
             //mandek ditolak
@@ -220,7 +217,6 @@ function display() {
         //ganti status asep or tidak
         if (state === acceptingState[document.getElementById("nav").textContent] || !obah) {
             statusOutput.style.background = (state === acceptingState[document.getElementById("nav").textContent] ? "green" : "red");
-            tapeOutput2.style.background = (state === acceptingState[document.getElementById("nav").textContent] ? "green" : "red");
             statusOutput.textContent = (state === acceptingState[document.getElementById("nav").textContent] ? "Accepted" : "Rejected");
         }
         stepCountOutput.textContent = stepCount;
@@ -451,7 +447,6 @@ function reset() {
     tapeOutput2.textContent = "";
     statusOutput.textContent = "";
     statusOutput.style.background = "";
-    tapeOutput2.style.background = "";
 
     //ora mlayu
     mlayu = false;
@@ -779,43 +774,6 @@ function factorial() {
         case 16:
             if (tape1[head1] === '0' && tape2[head2] === '0') action('0', '0', 'L', 'S', 16);
             else if (tape1[head1] === '0' && tape2[head2] === 'B') action('0', 'B', 'R', 'S', acceptingState[document.getElementById("nav").textContent]);
-            break;
-    }
-}
-
-function binaryLogarithm() {
-    switch (state) {
-        case 1:
-            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', '1', 'B', 'R', 'R', 'S', 2);
-            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
-            break;
-        case 2:
-            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', '0', 'B', 'R', 'R', 'S', 3);
-            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
-            break;
-        case 3:
-            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', 'B', 'R', 'S', 'S', 4);
-            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '0', 'S', 'L', 'R', 5);
-            break;
-        case 4:
-            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', '0', 'B', 'R', 'R', 'S', 3);
-            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '0', 'S', 'L', 'R', 5);
-            break;
-        case 5:
-            if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('1', 'B', 'B', 'L', 'L', 'S', 6);
-            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
-            break;
-        case 6:
-            if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
-            else if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('0', 'B', 'B', 'L', 'L', 'S', 7);
-            break;
-        case 7:
-            if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('B', 'B', 'B', 'S', 'L', 'S', 8);
-            else if (tape1[head1] === 'B' && tape2[head2] === "1" && tape3[head3] === "B") action('B', 'B', '0', 'R', 'S', 'R', 1);
-            break;
-        case 8:
-            if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('0', 'B', 'B', 'L', 'L', 'S', 7);
-            else if (tape1[head1] === 'B' && tape2[head2] === "1" && tape3[head3] === "B") action('B', 'B', '0', 'R', 'S', 'R', 1);
             break;
     }
 }
--- MultiTape/Multi3Tape.js
+++ MultiTape/Multi3Tape.js
@@ -122,6 +122,9 @@ function step() {
                 case "Division":
                     division();
                     break;
+                case "Binary Logarithm":
+                    binaryLogarithm();
+                    break;
             }
 
             //mandek ditolak
@@ -234,7 +237,6 @@ function display() {
         //ganti status asep or tidak
         if (state === acceptingState[document.getElementById("nav").textContent] || !obah) {
             statusOutput.style.background = (state === acceptingState[document.getElementById("nav").textContent] ? "green" : "red");
-            tapeOutput3.style.background = (state === acceptingState[document.getElementById("nav").textContent] ? "green" : "red");
             statusOutput.textContent = (state === acceptingState[document.getElementById("nav").textContent] ? "Accepted" : "Rejected");
         }
         stepCountOutput.textContent = stepCount;
@@ -536,7 +538,6 @@ function reset() {
     tapeOutput3.textContent = "";
     statusOutput.textContent = "";
     statusOutput.style.background = "";
-    tapeOutput3.style.background = "";
 
     //ora mlayu
     mlayu = false;
@@ -667,6 +668,43 @@ function division() {
             break;
         case 9:
             if (tape1[head1] === 'B' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
+            break;
+    }
+}
+
+function binaryLogarithm() {
+    switch (state) {
+        case 1:
+            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', '1', 'B', 'R', 'R', 'S', 2);
+            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
+            break;
+        case 2:
+            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', '0', 'B', 'R', 'R', 'S', 3);
+            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
+            break;
+        case 3:
+            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', 'B', 'R', 'S', 'S', 4);
+            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '0', 'S', 'L', 'R', 5);
+            break;
+        case 4:
+            if (tape1[head1] === '0' && tape2[head2] === "B" && tape3[head3] === "B") action('B', '0', 'B', 'R', 'R', 'S', 3);
+            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '0', 'S', 'L', 'R', 5);
+            break;
+        case 5:
+            if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('1', 'B', 'B', 'L', 'L', 'S', 6);
+            else if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
+            break;
+        case 6:
+            if (tape1[head1] === '1' && tape2[head2] === "B" && tape3[head3] === "B") action('B', 'B', '1', 'S', 'S', 'R', acceptingState[document.getElementById("nav").textContent]);
+            else if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('0', 'B', 'B', 'L', 'L', 'S', 7);
+            break;
+        case 7:
+            if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('B', 'B', 'B', 'S', 'L', 'S', 8);
+            else if (tape1[head1] === 'B' && tape2[head2] === "1" && tape3[head3] === "B") action('B', 'B', '0', 'R', 'S', 'R', 1);
+            break;
+        case 8:
+            if (tape1[head1] === 'B' && tape2[head2] === "0" && tape3[head3] === "B") action('0', 'B', 'B', 'L', 'L', 'S', 7);
+            else if (tape1[head1] === 'B' && tape2[head2] === "1" && tape3[head3] === "B") action('B', 'B', '0', 'R', 'S', 'R', 1);
             break;
     }
 }
